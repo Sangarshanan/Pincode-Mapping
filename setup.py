03a904edb503d2cb7d2d5506b0b3c533072a57ba
Sangarshanan <sangarshanan1998@gmail.com>
2019-02-13 12:39:41 +0530

    firstpackagecommit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pincodicem",
+    version="0.0.1",
+    author="Sangarshanan",
+    author_email="sangarshanan1998@gmail.com",
+    description="Convert LatLongs to Pincode ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sangarshanan/Pincodicem",
+    packages= ['citypincodes'],
+
+)
